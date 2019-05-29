a0550b68a351d1eb7bf9f1dac80bde6b254e6022
Alex Smith <smithavt14@gmail.com>
2019-05-29 12:49:17 +0700

    Added edit btn for personal information and edit / delete btn for experience

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,11 +1,14 @@
-// pages/edit/edit.js
-Page({
+import Toast from '../../miniprogram_npm/vant-weapp/toast/toast';
 
+Page({
   data: {
     user: null,
     profile: null,
     experience: null,
     education: null,
+    updateProfile: null,
+    updateExperience: null,
+    updateEducation: null,
     backgroundImages: ['https://cloud-minapp-27532.cloud.ifanrusercontent.com/1hU3JlR3qdI7ihhh.png', 'https://cloud-minapp-27532.cloud.ifanrusercontent.com/1hU3Jlj0b53ZoWxO.png', 'https://cloud-minapp-27532.cloud.ifanrusercontent.com/1hU3tom2f94O95Xj.png', 'https://cloud-minapp-27532.cloud.ifanrusercontent.com/1hU3toD51DPYYaSa.png']
   },
 
@@ -91,6 +94,73 @@ Page({
       this.getUserProfile(this.data.user)
     }, err => {
       console.log(err)
+    })
+  },
+
+  chooseAvatar() {
+    let self = this
+    wx.chooseImage({
+      count: 1,
+      success: function(res) {
+        let MyFile = new wx.BaaS.File()
+        let fileParams = {filePath: res.tempFilePaths[0]}
+        let metaData = {categoryName: 'SDK'}
+        
+        MyFile.upload(fileParams, metaData).then(res => {
+          let path = res.data.path
+          let Profile = new wx.BaaS.TableObject('profile')
+          let userProfile = Profile.getWithoutData(self.data.profile.id)
+          
+          userProfile.set('avatar', path)
+          userProfile.update().then(() => {
+            self.getUserProfile(self.data.user)
+          })
+        }, err => {
+          console.log(err)
+        })
+      }, 
+      fail: function(err) {
+        console.log(err)
+      }
+    })
+  },
+
+  updateValue(e) {
+    this.setData({
+      [`${e.target.dataset.group}.${e.target.dataset.name}`]: e.detail
+    })
+  },
+
+  updateProfile(e) {
+    let Profile = new wx.BaaS.TableObject('profile')
+    let userProfile = Profile.getWithoutData(this.data.profile.id)
+    userProfile.set(this.data.updateProfile)
+    userProfile.update().then(res => {
+      this.getUserProfile(this.data.user)
+      Toast.success('Success!')
+    })
+  },
+
+  updateExperience(e) {
+    let Experience = new wx.BaaS.TableObject('experience')
+    let experience = Experience.getWithoutData(e.target.dataset.id)
+    experience.set(this.data.updateExperience)
+    experience.update().then(res => {
+      this.getUserProfile(this.data.user)
+      Toast.success('Success!')
+    })
+  },
+
+  updateEducation(e) {
+
+  },
+
+  deleteExperience(e) {
+    let Experience = new wx.BaaS.TableObject('experience')
+    Experience.delete(e.target.dataset.id).then(res => {
+      console.log(res)
+      this.getUserProfile(this.data.user)
+      Toast.success('Success!')
     })
   },
 
